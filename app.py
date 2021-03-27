3e791a374202a45bc3bce18b241757eebccb0083
nartawat <73953418+nartawat@users.noreply.github.com>
2021-03-27 22:51:45 +0700

    make it better

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request
+from linebot import *
+from linebot.models import *
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('Lb5v+X5Hr1gdHa+6yFkWcxpPLlfCXVMqG+QXUIvjCN0HmftdNNl8NixyyDBmOGzVjNGzNGiB2koW10I8SEzSxijriRGZR1/G9n6FgqDvsLXrT2h++Y8LyYUYGadiOGixYj30nBHYnRAXwRmpdiU2FAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('97ea021104b6a2182af981ae780d0fb0')
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # body = request.get_data(as_text=True)
+    # print(body)
+    req = request.get_json(silent=True, force=True)
+    intent = req["queryResult"]["intent"]["displayName"]
+    text = req['originalDetectIntentRequest']['payload']['data']['message']['text']
+    reply_token = req['originalDetectIntentRequest']['payload']['data']['replyToken']
+    id = req['originalDetectIntentRequest']['payload']['data']['source']['userId']
+    disname = line_bot_api.get_profile(id).display_name
+
+    print('id = ' + id)
+    print('name = ' + disname)
+    print('text = ' + text)
+    print('intent = ' + intent)
+    print('reply_token = ' + reply_token)
+
+    reply(intent,text,reply_token,id,disname)
+
+    return 'OK'
+
+
+def reply(intent,text,reply_token,id,disname):
+    if intent == 'intent 2':
+        text_message = TextSendMessage(text='ทดสอบสำเร็จ')
+        line_bot_api.reply_message(reply_token,text_message)
+
+if __name__ == "__main__":
+    app.run()
