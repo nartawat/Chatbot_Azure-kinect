78bf05d1f803a4c384537a0999423f59361a5312
nartawat <73953418+nartawat@users.noreply.github.com>
2021-03-29 21:15:26 +0700

    change app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def callback():
 
 def reply(intent,text,reply_token,id,disname):
     if intent == 'intent 2':
-        text_message = TextSendMessage(text=json["text"]+"    "+ json["text"])
+        text_message = TextSendMessage(text="hello world")
         line_bot_api.reply_message(reply_token,text_message)
 
 # def replytae(intent,body,reply_token):
