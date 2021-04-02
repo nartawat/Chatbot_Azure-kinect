eb9f4c17062d5dc6879f2b82f0c0e69059086a21
nartawat <73953418+nartawat@users.noreply.github.com>
2021-04-02 20:02:58 +0700

    add Proc

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask, request
 from linebot import *
 from linebot.models import *
 
-# import pymongo as pm
-# from bson.objectid import ObjectId
+import pymongo
+from bson.objectid import ObjectId
 
-# client = pm.MongoClient() 
-# db = client['Chatbot']
-# collection = db['Azure']
+client = pymongo.MongoClient("mongodb+srv://chatbotazure:8ZDhcwsPc05AHYSr@cluster0.angf9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.Chatbot
+col = db["Azure"]
+
 
 app = Flask(__name__)
 
@@ -21,7 +22,7 @@ def hello():
 @app.route("/calltae", methods=['POST'])
 def calltae():
     body = request.get_json(silent=True, force=True)
-    # collection.update_one({'_id': ObjectId("60647c4cecf771397d12225e")},  {'$set': {"number": body["number"]}})
+    col.update_one({'_id': ObjectId("606714605bf23384453618dd")},  {'$set': {"number": body["number"]}})
     print(body["number"])
     
     return body
@@ -50,8 +51,8 @@ def callback():
 
 def reply(intent,text,reply_token,id,disname):
     if intent == 'intent 3':
-        # num = collection.find_one()
-        text_message = TextSendMessage(text="Heelo")
+        num = col.find_one()
+        text_message = TextSendMessage(text=num["number"])
         line_bot_api.reply_message(reply_token,text_message)
 
         
